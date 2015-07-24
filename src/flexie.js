9251197e2c4154b10b3222553aaa7dc181bd7356
evandegr <evandegr@gmail.com>
2015-07-24 14:40:33 -0400

    upgrading version

--- src/flexie.js
+++ src/flexie.js
@@ -2034,7 +2034,7 @@ var Flexie = (function (win, doc) {
 	FLX.version = "1.0.3";
 
 	// Load when the DOM is ready
-	attachLoadMethod(FLX.init);
+	 attachLoadMethod(FLX.init);
 
 	return FLX;
 }(window, document));
